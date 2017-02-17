cee0bc27fdde9b5a7190a15cfd46f34730e64aeb
韩婉茹 <wr.han@dface.cn>
2017-02-17 10:40:31 +0800

    project update

---
+++ src/components/Main.js
@@ -0,0 +1,51 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+
+let yeomanImage = require('../images/yeoman.png');
+
+// 获取图片相关数据
+let imageDatas = require('../data/imageData.json');
+
+// 利用自执行函数,将图片信息转成图片URL路径信息
+imageDatas = function genImageURL(imageDatasArr) {
+  for(var i = 0, j = imageDatasArr.length; i < j; i++) {
+    var singleImageData = imageDatasArr[i];
+
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+    imageDatasArr[i] = singleImageData
+  }
+  return imageDatasArr
+}(imageDatas);
+
+/*var GalleryByReactApp = React.createClass({
+  render: function () {
+    return (
+      <section className="stage">
+        <section className="img-sec">
+        </section>
+        <nav className="controller-nav>
+        </nav>
+      </section>
+    )
+  }
+})
+React.render(<GalleryByReactApp/>,document.getElementById('app'));*/
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+        <section className="img-sec">
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
